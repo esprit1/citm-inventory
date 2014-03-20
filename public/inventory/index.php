4115f75916c4145cdb72f12fb91e1079735663c1
esprit1 <kpavlov@gmail.com>
2014-03-19 20:04:05 -0400

    replaced index.html with index.php

---
+++ public/inventory/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" author="kp12k">
+		<title>CITM Inventory Management</title>
+	</head>
+
+	<body>	
+	<div id="content">
+		<div id="title">
+		CITM Inventory Management
+		</div>		
+	</div>
+	</body>
+</html>
